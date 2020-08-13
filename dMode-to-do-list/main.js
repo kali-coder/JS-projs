6ba90f821b8f9c6bade8f360305a64b7c2e4ea11
kali-coder <61048982+kali-coder@users.noreply.github.com>
2020-08-14 05:07:21 +0530

    JS-to-do-list
    with dark mode

---
+++ dMode-to-do-list/main.js
@@ -0,0 +1,77 @@
+//ennum practice pannanum
+
+var myNodeList = document.getElementsByTagName("li");
+for(var i = 0; i<myNodeList.length;i++){
+    var span = document.createElement("span");
+    var txt = document.createTextNode("\u00D7");
+    span.appendChild(txt);
+    span.className = "close";
+    myNodeList[i].appendChild(span)
+}
+
+var close = document.getElementsByClassName("close");
+for(var i = 0; i< close.length; i++){
+    close[i].onclick = function(){
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
+}
+
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev){
+    if(ev.target.tagName == 'LI'){
+        ev.target.classList.toggle('checked');
+    }
+},false);
+
+function addElement() {
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("myInput").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if(inputValue === ""){
+        alert("un computer na un ishta mayiruku pannuviya?");
+    } else{
+        document.getElementById("myUL").appendChild(li);
+    }
+      document.getElementById("myInput").value = "";
+
+      var span = document.createElement("span");
+      var txt = document.createTextNode("\u00D7");
+      span.appendChild(txt);
+      span.className = "close";
+      li.appendChild(span);
+
+    for(var i = 0; i< close.length; i++){
+        close[i].onclick = function(){
+            var div = this.parentElement;
+            div.style.display = "none";
+        }
+    }
+    
+}
+
+var ip = document.getElementById("myInput");
+ip.addEventListener("keydown",function(e){
+    if(e.keyCode == 13){
+        addElement();
+    }
+});
+
+
+function classChanger(id,classTochange) {
+    var element = document.getElementById(id);
+    element.classList.toggle(classTochange)
+    
+}
+
+function darkMode() {
+    classChanger("body","dark");
+    classChanger("myInput","d-input");
+    classChanger("header","d-header");
+    classChanger("myUL","d-ul");
+    classChanger("addBtn","d-addBtn");
+    classChanger("h1","d-h1");    
+}
+
+
